549b7dc945938008cc50affc4b74d26decf2e971
BoschLive <116604832+BoschLive@users.noreply.github.com>
2023-03-19 14:15:59 -0500

    Update script.js

--- script.js
+++ script.js
@@ -9,5 +9,6 @@ var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
 
 const marker = new google.maps.Marker({
   position:{lat:31.8026, lng:85.9572},
-  map:map
 });
+
+marker.setMap(map);
