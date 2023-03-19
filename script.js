ee723271462261e0246f92a15431e300bdc19159
BoschLive <116604832+BoschLive@users.noreply.github.com>
2023-03-19 16:10:05 -0500

    Update script.js

--- script.js
+++ script.js
@@ -9,14 +9,17 @@ let map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
 let marker1 = new google.maps.Marker({
   position:new google.maps.LatLng(31.802535609893273, -85.95787215346463)
 });
-
 marker1.setMap(map);
 
 let directionsService = new google.maps.DirectionsService();
 let directionsDisplay = new google.maps.DirectionsDirectionsDisplay();
 directionsDisplay.setMap(map);
 
+var onChangeHandler = function() {
+  calculateAndDisplayRoute(directionsService, directionsDisplay);
+};
+   document.getElementById('start').addEventListener('change', onChangeHandler);
+   document.getElementById('end').addEventListener('change', onChangeHandler);
 }
-  
   
 
