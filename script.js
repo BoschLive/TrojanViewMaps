201332ea67e1066095f44e67dbd4f1c982a9166f
BoschLive <116604832+BoschLive@users.noreply.github.com>
2023-03-19 16:58:47 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,17 +2,11 @@ function myMap() {
 let mapProp= {
   center:new google.maps.LatLng(31.802535609893273, -85.95787215346463),
   zoom:14,
- 
 };
 let map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
 
-let marker1 = new google.maps.Marker({
-  position:new google.maps.LatLng(31.802535609893273, -85.95787215346463)
-});
-marker1.setMap(map);
-
 let directionsService = new google.maps.DirectionsService();
-let directionsDisplay = new google.maps.DirectionsDirectionsDisplay();
+let directionsDisplay = new google.maps.DirectionsRenderer();
 directionsDisplay.setMap(map);
 
 let onChangeHandler = function() {
