1d5683a6e3b97c6323a59a4d92910adafc5ccd87
BoschLive <116604832+BoschLive@users.noreply.github.com>
2023-03-19 14:31:32 -0500

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let mapProp= {
 let map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
 
 const marker = new google.maps.Marker({
-  position:{lat:31.8026, lng:85.9572},
+  position:new google.maps.LatLng(31.802535609893273, -85.95787215346463),
   icon:'TroyU.png'
 });
 
