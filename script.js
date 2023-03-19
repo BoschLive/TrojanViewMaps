9636e1acdd5bc4f8ba2d431e3545d9e6909ce3e3
BoschLive <116604832+BoschLive@users.noreply.github.com>
2023-03-19 14:22:37 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function myMap() {
-var mapProp= {
+let mapProp= {
   center:new google.maps.LatLng(31.802535609893273, -85.95787215346463),
   zoom:14,
  
 };
-var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+let map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
 }
 
 const marker = new google.maps.Marker({
